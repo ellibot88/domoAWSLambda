a301e6b98cc50781b1caf5a323268d1c81f46bca
Elliott Leonard <egleonard88@gmail.com>
2022-05-15 14:44:37 -0600

    axios

--- teamsWriteback.js
+++ teamsWriteback.js
@@ -1,7 +1,9 @@
 //Authenticate with Domo API
 
-const domo_client_id = "domo_client_id";
-const domo_client_secret = "domo_client_secret";
+const axios = require("axios");
+const domo_client_id = "e0e7020f-2b8e-41e0-b847-4f28c224177c";
+const domo_client_secret =
+  "89c3cec0cafc3e361efb53d0d5da2faa86786ff72e435161ab1adf9e9eb582a8";
 
 const config = {
   headers: {
@@ -17,7 +19,25 @@ axios
     config
   )
   .then((res) => {
+    // console.log(res.data["access_token"])
     let token = res.data["access_token"];
-    return token;
+    const configuration = {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+    const data = {
+      sql: `SELECT * FROM table`,
+    };
+    axios
+      .post(
+        `https://api.domo.com/v1/datasets/query/execute/6c381347-9460-42cf-88de-835eae24b64a`,
+        data,
+        configuration
+      )
+      .then((axiosRes) => {
+        console.log(axiosRes.data.rows);
+      })
+      .catch((axiosErr) => console.log(axiosErr));
   })
   .catch((err) => console.log(err));
